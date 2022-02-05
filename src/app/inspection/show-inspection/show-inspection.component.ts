bcccc5c4a1ca9a2b1b5110ed819850829ee9199c
maalloor <maalloor@espol.edu.ec>
2022-02-05 18:43:23 -0500

    Creating the method to delete inspections

--- src/app/inspection/show-inspection/show-inspection.component.ts
+++ src/app/inspection/show-inspection/show-inspection.component.ts
@@ -65,4 +65,30 @@ export class ShowInspectionComponent implements OnInit {
       }
     });
   }
+
+  deleteInspection(item: any)
+  {
+    if (confirm(`Are you sure you want to delete inspection ${item.id}?`))
+    {
+      this.service.deleteInspection(item.id).subscribe(res => {
+        var closeModalBtn = document.getElementById('add-edit-modal-close');
+        if (closeModalBtn)
+        {
+          closeModalBtn.click();
+        }
+
+        var showDeleteSuccess = document.getElementById('delete-success-alert');
+        if (showDeleteSuccess)
+        {
+          showDeleteSuccess.style.display = "block";
+        }
+        setTimeout(function () {
+          if (showDeleteSuccess)
+          {
+            showDeleteSuccess.style.display = "none";
+          }
+        }, 4000);
+      });
+    }
+  }
 }
